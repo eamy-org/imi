366466d7bfb1912ad88916022a38f373aede2f9e
Vadim Gusev <vadim@whitestar.ru>
2014-12-12 03:52:42 +0300

    Server CLI

--- imi/bin/imi.py
+++ imi/bin/imi.py
@@ -1,4 +1,5 @@
-from bottle import run
+import argparse
+from bottle import run as run_bottle
 from ..daemon import Daemon
 from ..web import init_app
 from ..config import WEB_HOST, WEB_PORT, PIDFILE
@@ -6,15 +7,47 @@ from ..config import WEB_HOST, WEB_PORT, PIDFILE
 __all__ = ['main']
 
 
-class ImiDaemon(Daemon):
+class ServerDaemon(Daemon):
     def run(self):
-        server()
+        run_server()
 
 
-def server():
-    run(init_app(), host=WEB_HOST, port=WEB_PORT)
+def run_server():
+    run_bottle(init_app(), host=WEB_HOST, port=WEB_PORT)
+
+
+class ServerCli:
+    def __init__(self):
+        self.daemon = ServerDaemon(PIDFILE)
+
+    def start(self, detach=False):
+        if detach:
+            self.daemon.start()
+        else:
+            self.daemon.run()
+
+    def stop(self):
+        self.daemon.stop()
+
+    def restart(self):
+        self.daemon.restart()
 
 
 def main():
-    daemon = ImiDaemon(PIDFILE)
-    daemon.start()
+    server_cmd = ['start', 'stop', 'restart']
+    message_cmd = ['send']
+    commands = server_cmd + message_cmd
+    parser = argparse.ArgumentParser()
+    parser.add_argument('command', choices=commands)
+    parser.add_argument('--detach', action='store_true')
+    ns = parser.parse_args()
+    if ns.command in server_cmd:
+        server = ServerCli()
+        if ns.command == 'start':
+            server.start(ns.detach)
+        elif ns.command == 'stop':
+            server.stop()
+        elif ns.command == 'restart':
+            server.restart()
+    else:
+        pass
--- tests/bin/imi.py
+++ tests/bin/imi.py
@@ -1,39 +1,80 @@
 #!/usr/bin/env python
+import shlex
+import sys
 
 import unittest
-from unittest.mock import patch, sentinel, call
+from unittest.mock import patch, call
 
 from imi.bin import imi
 from imi.config import WEB_HOST, WEB_PORT
 
-__all__ = ['TestImi']
+__all__ = ['TestMainServer', 'TestMainMessage']
 
 
-class TestImi(unittest.TestCase):
+class TestMainServer(unittest.TestCase):
+
+    def setUp(self):
+        self.pidfile = '/test/path/test.pid'
+        pidfile_patch = patch('imi.bin.imi.PIDFILE', self.pidfile)
+        pidfile_patch.start()
+        self.addCleanup(pidfile_patch.stop)
+        deamon = patch('imi.bin.imi.ServerDaemon')
+        self.addCleanup(deamon.stop)
+        self.deamon = deamon.start()
+
+    def test_main_start_daemon(self):
+        sys.argv = shlex.split('imi start --detach')
+        imi.main()
+        expected = call(self.pidfile).start().call_list()
+        self.assertEqual(expected, self.deamon.mock_calls)
+
+    def test_main_start(self):
+        sys.argv = shlex.split('imi start')
+        imi.main()
+        expected = call(self.pidfile).run().call_list()
+        self.assertEqual(expected, self.deamon.mock_calls)
+
+    def test_main_stop(self):
+        sys.argv = shlex.split('imi stop')
+        imi.main()
+        expected = call(self.pidfile).stop().call_list()
+        self.assertEqual(expected, self.deamon.mock_calls)
+
+    def test_main_restart(self):
+        sys.argv = shlex.split('imi restart')
+        imi.main()
+        expected = call(self.pidfile).restart().call_list()
+        self.assertEqual(expected, self.deamon.mock_calls)
+
+    def tearDown(self):
+        pass
+
+
+class TestMainMessage(unittest.TestCase):
 
     def setUp(self):
         pass
 
-    @patch('imi.bin.imi.PIDFILE', '/test/path/test.pid')
-    @patch('imi.bin.imi.ImiDaemon')
-    def test_main(self, deamon):
+    def test_main_send(self):
+        sys.argv = shlex.split('imi send')
         imi.main()
-        expected = call('/test/path/test.pid').start().call_list()
-        self.assertEqual(expected, deamon.mock_calls)
 
-    @patch('imi.bin.imi.run')
+    def tearDown(self):
+        pass
+
+
+class TestServerDaemon(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    @patch('imi.bin.imi.run_bottle')
     @patch('imi.bin.imi.init_app')
-    def test_server(self, init_app, run):
-        init_app.return_value = sentinel.app
-        imi.server()
-        init_app.assert_called_once_with()
-        run.assert_called_once_with(sentinel.app, host=WEB_HOST, port=WEB_PORT)
-
-    @patch('imi.bin.imi.server')
-    def test_daemon(self, server):
-        deamon = imi.ImiDaemon('/test/path/test.pid')
-        deamon.run()
-        server.assert_called_once_with()
+    def test_run(self, init_app, run_bottle):
+        daemon = imi.ServerDaemon('/test/path/test.pid')
+        app = init_app.return_value
+        daemon.run()
+        run_bottle.assert_called_once_with(app, host=WEB_HOST, port=WEB_PORT)
 
     def tearDown(self):
         pass
